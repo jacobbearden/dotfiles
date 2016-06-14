fff47ff9f81d3ead72e399752d4900caa7f80fb1
Jacob Bearden <jacob.bearden0@gmail.com>
2016-06-14 01:03:53 -0600

    add sublime to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+
+	cp --symbolic-link sublime/* ~/.config/sublime-text-3/Packages/User -f
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
